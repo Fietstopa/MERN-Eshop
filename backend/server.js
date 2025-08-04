0ebdb001525bc324054a21a49202ea6d19484a74
Fietstopa <bmisko984@gmail.com>
2025-08-04 16:29:38 +0200

    env fix

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 dotenv.config();
@@ -12,6 +14,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   console.log("running on " + PORT);
