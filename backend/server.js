78219491c53ecf098ac187a8f08e540dc8a0091c
Fietstopa <bmisko984@gmail.com>
2025-08-06 19:43:20 +0200

    stripe integration

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,10 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
-
+import couponRoutes from "./routes/coupon.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import paymentRoutes from "./routes/payment.route.js";
 dotenv.config();
 
 const app = express();
@@ -17,7 +18,8 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
-
+app.use("/api/coupons", couponRoutes);
+app.use("api/payments", paymentRoutes);
 app.listen(PORT, () => {
   console.log("running on " + PORT);
   connectDB();
